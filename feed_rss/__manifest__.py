ca9c2cc017eb4406907d35f0d148d793126c22c0
Darío Lodeiros <dario@commitsun.com>
2024-11-07 10:19:58 +0100

    [WIP]feed_rss: manifest 16

--- feed_rss/__manifest__.py
+++ feed_rss/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Feed RSS",
-    "version": "14.0.2.6.7",
+    "version": "16.0.1.0.0",
     "author": "Commit [Sun], Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": True,
